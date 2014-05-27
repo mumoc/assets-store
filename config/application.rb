7ab55dc1788bead32747083debe580dee850eadd
Mumo Carlos <mumo.crls@gmail.com>
2014-05-27 14:20:48 -0500

    Adds missing files

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(*Rails.groups)
 
 module AssetsStore
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('templates')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
